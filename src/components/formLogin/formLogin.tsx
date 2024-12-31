9eefa3f63a12845c21bd2d7bd3d40968bc6fbf1d
Julian Otalvaro <jotal8@gmail.com>
2024-12-31 00:30:19 -0500

    Se quita error de puerto que se quedo por error quemado en codigo, ya se encuentra listo para desplegar

--- src/components/formLogin/formLogin.tsx
+++ src/components/formLogin/formLogin.tsx
@@ -19,7 +19,7 @@ const FormLogin = () => {
     const credenciales = { email, password };
 
   try {
-    const response = await fetch('http://localhost:3000/api/login', {
+    const response = await fetch(`/api/login`, {
       method: 'POST',
       headers: {
         'Content-Type': 'application/json'
